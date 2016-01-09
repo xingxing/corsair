ccf89cc211d04e2d03b00ca03ea0b9532430988c
xingxing <xingxing@jd.com>
2016-01-09 21:29:29 +0800

    first commit: migrate from MacOS

---
+++ init.el
@@ -0,0 +1,23 @@
+(setq user-full-name "Wade Xing")
+(setq user-mail-address "iamxingxing@gmail.com")
+
+(delete-selection-mode t)
+
+(ido-mode t)
+
+(setq column-number-mode t)
+
+(setq inhibit-statup-message t)
+
+(display-time-mode t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(show-paren-mode t)
+
+(server-start)
+
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+
