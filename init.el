5078b74b11c4a1ddc3dbfff04e328a17ae49f0bd
xingxing <xingxing@jd.com>
2016-01-12 20:50:52 +0800

    smex and undo-tree

--- init.el
+++ init.el
@@ -47,3 +47,8 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; undo-tree
+(add-to-list 'load-path "~/.emacs.d/vendor/undo-tree/")
+(require 'undo-tree)
+(global-undo-tree-mode)
